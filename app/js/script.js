157e097eb40542a5b914b81851f2f92dc7d30d09
Nnadi Victory <chikamagozi25@gmail.com>
2023-02-01 22:15:26 +0100

    first init

---
+++ app/js/script.js
@@ -0,0 +1,6 @@
+// fetch('./data.json')
+//     .then(res => res.json()) 
+//     .then(data => {
+//         console.log(data)
+//     })
+//     .catch(err => console.log('something went wrong', err))
