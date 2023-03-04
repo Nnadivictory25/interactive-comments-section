8a72dd2ba8df01e5c39c46607a9671051a7a01a9
Nnadi Victory <chikamagozi25@gmail.com>
2023-03-04 19:41:20 +0100

    fixed deleting comments not working

--- app/js/script.js
+++ app/js/script.js
@@ -3,14 +3,20 @@ const overlay = document.querySelector(".overlay");
 const body = document.querySelector("body");
 const commentsCtn = document.querySelector(".allCommentCtn");
 const userImgEl = document.querySelector("#currentUserImg");
+let heights = []
+let replyToDelete;
 
+
+const generateUniqueId = () => {
+  return Date.now().toString(36) + Math.random().toString(36).substring(2, 5);
+}
 
 const openModal = (element) => {
   modal.classList.add("fade-in");
   overlay.classList.add("fade-in");
   modal.style.display = "block"; // make the modal visible
   overlay.style.display = "block"; // make the overlay visible
-  body.classList.toggle("no-scroll");
+  body.classList.add("no-scroll");
 
   overlay.addEventListener("click", closeModal);
 };
@@ -18,7 +24,7 @@ const openModal = (element) => {
 const closeModal = () => {
   modal.classList.add("fade-out");
   overlay.classList.add("fade-out");
-  body.classList.toggle("no-scroll");
+  body.classList.remove("no-scroll");
 
   setTimeout(() => {
     modal.style.display = "none"; // hide the modal after the animation completes
@@ -63,8 +69,9 @@ if (commentData.length === 0) {
 
 // ! RENDER COMMENTS FUNCTION
 const renderComments = () => {
+  console.log("renderComments function called");
+
   commentsCtn.innerHTML = "";
-  console.log("working");
 
   commentData.comments.forEach((comment, i) => {
     const { content, createdAt, replies, score, upvoted, downvoted, user } = comment;
@@ -139,7 +146,16 @@ const renderComments = () => {
     </section>
         `;
      }
-  });
+  })
+
+
+  commentData.comments.forEach((comment, i) => {
+    const { replies } = comment
+    if (replies.length > 0) {
+      getSpaceBetweenElements(i)
+      heights = []
+    }
+  })
 };
 
 // ! DATE FORMAT CHECKER
@@ -150,14 +166,12 @@ const isDate = (value) => {
 
 // ! REPLY RENDERING FUNCTION 
 const renderReplies = (data, commentIndex) => {
-
+  console.log("render replies function called");
   let html = "";
   if (data.length > 0) {
-    getSpaceBetweenElements(commentIndex)
 
     data.forEach((reply, i) => {
-      const { content, createdAt, replyingTo, score, upvoted, downvoted, user } = reply;
-
+      const { id, content, createdAt, replyingTo, score, upvoted, downvoted, user } = reply;
 
         if (user.username !== currentUser.username) {
             html += /*html*/ `
@@ -220,11 +234,11 @@ const renderReplies = (data, commentIndex) => {
                     <i  onclick="decreaseReplyScore(${i}, '${commentIndex}', this, ${downvoted})" class="bi bi-dash"></i>
                 </div>
                 <div class="crudBtns flex items-center gap-x-3 lg:gap-x-5 font-medium lg:absolute top-7 right-10">
-                    <div onclick="openModal(this)" class="delete flex items-center gap-x-1 text-red-500 cursor-pointer">
+                    <div onclick="deleteReply(this, ${commentIndex}, ${i}, '${id}')" class="delete flex items-center gap-x-1 text-red-500 cursor-pointer">
                         <i class="bi bi-trash-fill"></i>
                         <p>DELETE</p>
                     </div>
-                    <div onclick="editReply(this, ${commentIndex}, ${i}, ${score})" class="edit flex items-center gap-x-1 cursor-pointer">
+                    <div onclick="editReply(this, ${commentIndex}, ${i}, ${score}, '${isDate(createdAt) ? formatDate(createdAt) : createdAt}', '${replyingTo}')" class="edit flex items-center gap-x-1 cursor-pointer">
                         <i class="bi bi-pen-fill"></i>
                         <p>EDIT</p>
                     </div>
@@ -240,13 +254,22 @@ const renderReplies = (data, commentIndex) => {
 };
 
 
-let heights = []
+
 const getSpaceBetweenElements = (commentIndex) => {
+    console.log("getSpaceBetweenElements function called");
     let element1;
     let element2;
     let spaceBetweenElements;
+    let length = commentData.comments.length
 
     setTimeout(() => {
+        commentData.comments.forEach((comment, i) => {
+          const { replies } = comment
+          if (replies.length == 0) {
+            length--
+          }
+        })
+        console.log(length)
         const lines = Array.from(document.querySelectorAll('.line'))
         element1 = document.getElementById(`${commentIndex}`);
         if (commentIndex !== commentData.comments.length - 1) {
@@ -257,7 +280,7 @@ const getSpaceBetweenElements = (commentIndex) => {
         spaceBetweenElements = element2.offsetTop - (element1.offsetTop + element1.offsetHeight)
         heights.push(spaceBetweenElements)
         console.log(heights)
-        for (let i = 0; i < heights.length; i++) {
+        for (let i = 0; i < length; i++) {
           lines[i].style.height = `${heights[i]}px`;
         }
     });
@@ -317,8 +340,8 @@ const decreaseReplyScore = (replyIndex, commentIndex, element, downvoted) => {
 }
 
 window.addEventListener('resize', () => {
-  heights = []
   commentData.comments.forEach((comment, i) => {
+    heights = []
     const { replies } = comment
     if (replies.length > 0) {
       getSpaceBetweenElements(i)
@@ -331,8 +354,25 @@ const reply = (element, commentIndex, isOnReply, replyingTo) => {
   let commentCard;
   heights = []
 
+    if (!isOnReply) {
+        commentCard =  element.closest(".commentSection");
+    } else {
+        commentCard = element.closest(".replySection");
+    }
+    
+    const commentCardChildren = Array.from(commentCard.children);
+    const index  = commentCardChildren.length - 1;
+    
+
     let html = /*html*/ `
     <div class="addCommentCard bg-white px-5 lg:pr-4 rounded-xl flex flex-col lg:justify-end lg:flex-row gap-x-5 relative lg:pb-6 lg:pt-4 w-[95%] ml-auto lg:w-[88%] mt-5">
+        ${
+          index === 0
+            ? /*html*/ `
+        <span id="line" class="block line absolute top-0 w-[2px] left-[-.95rem] lg:left-[-3rem] bg-slate-300"></span>
+        `
+            : ""
+        }
         <textarea name="comment" id="${isOnReply ? 'replyInput' : 'replyToCommentInput'}" cols="30" rows="3" class="textArea w-full px-4 py-1 mt-5 lg:mt-4 rounded-lg lg:w-[70%]" placeholder="Add a comment...">@${replyingTo + ''} </textarea>
         <div class="footer flex justify-between items-center lg:block py-3">
         <img class="w-10 lg:absolute left-5 top-8" src="${currentUser.image.webp}" alt="">
@@ -340,11 +380,6 @@ const reply = (element, commentIndex, isOnReply, replyingTo) => {
         </div>
     </div>
     `
-    if (!isOnReply) {
-        commentCard =  element.closest(".commentSection");
-    } else {
-        commentCard = element.closest(".replySection");
-    }
     
    const lastChild = commentCard.lastElementChild;
      
@@ -372,7 +407,6 @@ const formatDate = (date, locale = navigator.language) => {
   const calcDaysPassed = (date1, date2) => Math.round(Math.abs(date2 - date1) / (1000 * 60 * 60 * 24))
 
   const daysPassed = calcDaysPassed(new Date(), date)
-
   switch (daysPassed) {
     case 0:
       return 'Today';
@@ -396,6 +430,7 @@ const formatDate = (date, locale = navigator.language) => {
 }
 
 const preRenderReply = (element, replyingTo, isOnReply) => {
+  heights = []
   let inputValue = element.parentElement.previousElementSibling.value.trim()
   inputValue = inputValue.replace(`@${replyingTo}`, "");
 
@@ -408,7 +443,8 @@ const preRenderReply = (element, replyingTo, isOnReply) => {
   }
 
   commentToInsertIn.replies.push({
-    content: inputValue,
+    id: generateUniqueId(),
+    content: inputValue.trim(),
     createdAt: formatDate(new Date()),
     score: 0,
     replyingTo: replyingTo,
@@ -421,44 +457,46 @@ const preRenderReply = (element, replyingTo, isOnReply) => {
     },
   });
 
-  heights = []
   updatelocalstorage()
   renderComments()
-
-  commentData.comments.forEach((comment, i) => {
-    const { replies } = comment
-    if (replies.length > 0) {
-      getSpaceBetweenElements(i)
-    }
-  })
 }
 
+let replyToUpdate;
+let inputValue;
 
-const editReply = (element, commentIndex, replyIndex, score) => {
+const editReply = (element, commentIndex, replyIndex, score, createdAt, replyingTo) => {
   const comment = commentData.comments.find(comment => comment.id === commentIndex + 1)
   const thisReplies = comment.replies
   const reply = thisReplies.find(reply => thisReplies.indexOf(reply) === replyIndex)
-  const inputValue = element.parentElement.parentElement.previousElementSibling.textContent.trim()
+  replyToUpdate = reply
+  inputValue = element.parentElement.parentElement.previousElementSibling.textContent.trim().replace(`@${replyingTo}`, "")
   const commentCard = element.closest(".commentCard")
   const commentSection = element.closest(".commentSection")
-  
+
   let html = /*html*/ `
   <div class="commentCard bg-white rounded-xl p-5 relative lg:pr-20 lg:pl-24 lg:pb-4 mt-5 ml-auto w-[95%] lg:w-[88%]">
+    ${
+      replyIndex === 0
+        ? /*html*/ `
+    <span id="line" class="block line absolute top-0 w-[2px] left-[-.95rem] lg:left-[-3rem] bg-slate-300"></span>
+    `
+        : ""
+    }
     <div class="commentCard__header flex items-center gap-x-4 pb-4">
     <img class="w-10" src="${currentUser.image.webp}" alt="user profile avatar">
       <p class="username font-semibold">juliosumo</p>
       <p class="you text-white px-2 ml-[-10px] text-sm bg-blue">you</p>
-      <p class="postedAt font-medium">2 days ago</p>
+      <p class="postedAt font-medium">${createdAt}</p>
       </div>
-    <textarea name="comment" id="update" cols="30" rows="4" class="w-full px-4 py-2 mt-4 lg:mt-2 rounded-lg mb-3 lg:mb-0">${inputValue}</textarea>
+    <textarea oninput="validateInput(this)" name="comment" id="update" cols="30" rows="4" class="w-full px-4 py-2 rounded-lg mb-3 lg:mb-0">@${replyingTo} ${inputValue}</textarea>
     <div class="commentCard__footer flex items-center justify-between mt-5 lg:block">
       <div class="score bg-grey flex items-center gap-x-3 font-medium rounded-lg px-4 py-2 lg:absolute left-7 lg:flex-col top-5 lg:py-4 lg:px-3">
         <i class="bi bi-plus"></i>
         <span class="count">${score}</span>
-        <i class="bi bi-dash"></i>
+        <i class="bi bi-dash"></i> 
       </div>
         <div class="updateBtn lg:text-right">
-          <button onclick="updateReply(this)" class="px-7 py-3 lg:py-2 lg:mt-1 text-white rounded-lg font-medium">UPDATE</button>
+          <button disabled onclick="updateReply(this, '${replyingTo}')" id="updateBtn" class="px-7 py-3 lg:py-2 lg:mt-1 text-white rounded-lg font-medium disabled:opacity-30 disabled:pointer-events-none">UPDATE</button>
         </div>
       </div>
     </div>
@@ -477,6 +515,52 @@ const editReply = (element, commentIndex, replyIndex, score) => {
 
   const textArea = document.getElementById("update")
   textArea.focus();
-  textArea.selectionStart = inputValue.length + 1;
-  textArea.selectionEnd = inputValue.length + 1;
+  textArea.selectionStart = textArea.value.length + 1;
+  textArea.selectionEnd = textArea.value.length + 1;
 }
+
+const validateInput = (input) => {
+  const btn = document.getElementById("updateBtn")
+  if (input.value === inputValue) {
+    btn.disabled = true;
+  } else {
+    btn.disabled = false;
+  }
+} 
+
+const updateReply = (element, replyingTo) => {
+  heights = []
+  const inputValue = element.parentElement.parentElement.previousElementSibling.value.trim().replace(`@${replyingTo}`, "")
+  replyToUpdate.content = inputValue
+  replyToUpdate.createdAt = formatDate(new Date())
+  updatelocalstorage()
+  renderComments()
+}
+
+const deleteReply = (element, commentIndex, replyIndex, id) => {
+  const comment = commentData.comments.find(comment => comment.id === commentIndex + 1)
+  let thisReplies = comment.replies
+  const index = thisReplies.findIndex(reply => reply.id === id);
+  console.log(index)
+  if (index === -1) {
+    // reply not found
+    return;
+  }
+
+  openModal()
+
+  const deleteBtn = document.getElementById("deleteBtn")
+
+  deleteBtn.addEventListener("click", () => { 
+    element.parentElement.parentElement.parentElement.remove()
+    thisReplies.splice(index, 1)
+    console.log(commentData.comments)
+    setTimeout(() => {
+      updatelocalstorage()
+      renderComments()
+    });
+    closeModal()
+  })
+}
+
+
